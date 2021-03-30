317d05f62525941a5a6a1842d8f3bdff30a2211a
saileshchauhan <chauhansailesh68@gmail.com>
2021-03-30 12:46:18 +0530

    Refactor UC4

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,26 +1,30 @@
 #!/bin/bash/ -x
 
-
-
 heads=0
 tails=0
 
+function flipcoin() {
+	 result=$((RANDOM % 2))
+   if [ $result -eq 0 ]
+   then
+      heads=$(($heads+1))
+   else
+      tails=$(($tails+1))
+   fi
+}
+
+
 while [ $heads -ne 21 -a $tails -ne 21 ]
 do
-
-	result=$((RANDOM % 2))
-	if [ $result -eq 0 ]
-	then
-		heads=$(($heads+1))
-	else
-		tails=$(($tails+1))
-	fi
+	flipcoin
 done
 
 if [ $heads -eq $tails ]
 then
-	echo "Tie : Heads "$heads
-	echo "Tie : Tails "$tails
+	while [ $(($heads-$tails)) -ne 2 -a $(($tails-$heads)) -ne 2 ]
+	do
+	flipcoin
+	done
 elif [ $heads -gt $tails ]
 then
 	echo "Heads Win " "Difference :"$(($heads-$tails))
